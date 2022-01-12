f0846e159f984bc16923c7f8c5cd914526b9d358
jewoo <jeus0630@designfever.com>
2022-01-12 15:29:03 +0900

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
-import Link from "next/link";
-import {useRouter} from "next/router";
-
 function Home() {
-
-    const router = useRouter();
-
-    const handleClick = () => {
-        router.push('/product');
-    }
 
     return  (
         <div>
-            <Link href={"/blog/first"}>
-                <a>
-                    Blog
-                </a>
-            </Link>
-            <Link href={"/product"}>
-                <a>
-                    Product
-                </a>
-            </Link>
-            <button onClick={handleClick}>
-                Place Order
-            </button>
         </div>
     )
 }
