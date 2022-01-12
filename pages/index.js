7d13148d64e6a917808851e74b5ec79203bd84f1
jewoo <jeus0630@designfever.com>
2022-01-12 14:07:54 +0900

    Link / Router.push() 404

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,30 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 function Home() {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/product');
+    }
+
     return  (
-        <h1>Home Page</h1>
+        <div>
+            <Link href={"/blog/first"}>
+                <a>
+                    Blog
+                </a>
+            </Link>
+            <Link href={"/product"}>
+                <a>
+                    Product
+                </a>
+            </Link>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
+        </div>
     )
 }
 
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Link from "next/link";
 
-const Product = () => {
+const Product = ({productId = 100}) => {
     return (
         <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <Link href={"/"}>
+                <a>
+                    Home
+                </a>
+            </Link>
+            <h2>
+                <Link href={"/product/1"}>
+                    <a>Product1</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={"/product/2"}>
+                    <a>Product2</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={"/product/3"}>
+                    <a>Product3</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={`/product/${productId}`}>
+                    <a>Product {productId}</a>
+                </Link>
+            </h2>
         </>
     );
 };
