76cbb97b43db5c6ae9eb59033319fc09329ab087
jewoo <jeus0630@designfever.com>
2022-01-13 13:41:05 +0900

    getStaticProps fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {useRouter} from "next/router";
 
 const Post = ({post}) => {
+
+    const router = useRouter();
+
+    if(router.isFallback){
+        return (
+            <h1>Loading...</h1>
+        )
+    }
 
     return (
         <div>
@@ -14,13 +23,23 @@ export const getStaticPaths = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
 
-    const paths = data.map(el=>({
-        params : {postId : `${el.id}`}
-    }))
+    // const paths = data.map(el=>({
+    //     params : {postId : `${el.id}`}
+    // }))
 
     return {
-        paths,
-        fallback : false
+        paths : [
+            {
+                params : {postId : 1}
+            },
+            {
+                params : {postId : 2}
+            },
+            {
+                params : {postId : 3}
+            }
+        ],
+        fallback : true
     }
 }
 
@@ -30,9 +49,15 @@ export const getStaticProps = async (context) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
 
+    if(!data.id){
+        return {
+            notFound : true
+        }
+    }
+
     return {
         props : {
-            post : data
+            post : data.slice(0,3)
         }
     }
 }
