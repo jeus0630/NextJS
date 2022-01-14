51e06aaeb781d48115e0ee57a6de1e4fb84e0612
jewoo <jeus0630@designfever.com>
2022-01-14 16:41:29 +0900

    App Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import '../styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Header></Header>
+        <Component {...pageProps} />
+        <Footer></Footer>
+      </>
+  )
 }
 
 export default MyApp
